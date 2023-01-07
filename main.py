0ea02f122d458d31c1826dd33133542b538f94ca
pixcelo <bodomu@gmail.com>
2023-01-08 06:37:06 +0900

    update

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ url = 'https://coincheck.com/api/ticker'
 
 r = requests.get(url)
 print(r.json())
+#  {'last': 2239853.0, 'bid': 2239604.0, 'ask': 2240041.0, 'high': 2245372.0, 'low': 2237000.0, 'volume': 364.79393303, 'timestamp': 1673127354}
